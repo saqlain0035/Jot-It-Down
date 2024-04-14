510df69f83fde04c2c07617af92c8d4d75504e1b
[saqlain0035] <[saqlain0035@gmail.com]>
2024-04-14 10:47:48 +0530

    updated patter

--- newUser.php
+++ newUser.php
@@ -280,8 +280,8 @@ if(isset($_POST['submit']) && isset($_POST['un']) && isset($_POST['fn']) && isse
     ?>
     <form action="" method="post">
         <p>Username: <input type="text" name="un" pattern="[a-zA-Z0-9_]+" title="Username can only contain letters, numbers, and underscores." value="<?= $un ?>" required></p>
-        <p>First name: <input type="text" name="fn" pattern="[a-zA-Z]+" title="Your name can only contain letters." value="<?= $f ?>" required></p>
-        <p>Last name: <input type="text" name="ln" pattern="[a-zA-Z]+" title="Your name can only contain letters." value="<?= $l ?>"></p>
+        <p>First name: <input type="text" name="fn" pattern="[a-zA-Z ]+" title="Your name can only contain letters." value="<?= $f ?>" required></p>
+        <p>Last name: <input type="text" name="ln" pattern="[a-zA-Z ]+" title="Your name can only contain letters." value="<?= $l ?>"></p>
         <p>Password: <input type="password" name="ps1" required></p>
         <p>Password again: <input type="password" name="ps2" required></p>
         <p>Email: <input type="email" name="em" value="<?= $e ?>" required></p>
